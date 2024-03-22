2a837256cef05ca75968c840a52e53ff072e09d0
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-03-22 07:36:28 -0300

    Add files via upload

---
+++ numeros aleatorios.py
@@ -0,0 +1,5 @@
+import random
+numero_minimo=int(input("digite o valor minimo"))
+numero_maximo=int(input("digite o valor maximo"))
+numero_aleatorio=random.randint(numero_minimo,numero_maximo)
+print("o numero aleatorio e",numero_aleatorio)
---
+++ par..py
@@ -0,0 +1,7 @@
+num=int(input("digite um numero"))
+
+if num % 2 == 0:
+
+ print("o numero e par")
+else:
+ print("o numero e impar")
